c5f717629e5b4609459128da37ac5cce60cac646
JM-LIM99 <yiuison12@gmail.com>
2026-06-10 00:00:29 +0900

    changed

---
+++ Json_traite.py
@@ -0,0 +1,6 @@
+import json
+
+with open("/Users/jungmin/Desktop/gripic_sorbonne/gripic-sorbonne.github.io/GRIPIC_last_publis.json", "r") as f:
+    json_data = json.load(f)
+
+    print(json_data["type"])
